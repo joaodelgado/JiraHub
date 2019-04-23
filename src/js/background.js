3da1256cecb2c2d64a926b6d31c0321a7bda0b04
João Delgado <joao.delgado@jumia.com>
2019-04-23 14:23:01 +0100

    Add HeadersReceived listener to circumvent CORB

--- src/js/background.js
+++ src/js/background.js
@@ -14,7 +14,32 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
         }
         return { requestHeaders: headers };
     }, {
-        urls: ['https://jira.jumia.com/*'],
+        urls: ['https://jira.jumia.com/rest/api/*'],
     },
     ['requestHeaders', 'blocking']
 );
+
+chrome.webRequest.onHeadersReceived.addListener(
+    (details) => {
+        const headers = details.responseHeaders;
+        let i = 0;
+        for (i = 0; i < headers.length; ++i) {
+            if (headers[i].name === 'User-Agent') {
+                break;
+            }
+        }
+        if (i < headers.length) {
+            headers[i] = {
+                name: 'Access-Control-Allow-Origin',
+                value: '*',
+            };
+        } else {
+            headers.push({
+                name: 'Access-Control-Allow-Origin',
+                value: '*',
+            });
+        }
+        return { responseHeaders: headers };
+    }, {
+        urls: ['https://jira.jumia.com/rest/api/*'],
+    }, ['blocking', 'responseHeaders', 'extraHeaders']);
